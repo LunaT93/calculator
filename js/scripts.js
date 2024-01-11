85638987b3255ed8d8ac39372f74824dce3f93d3
LunaThomas69 <151404103+LunaThomas69@users.noreply.github.com>
2024-01-11 10:27:03 -0800

    Simple Calculator
    A very simple calculator.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,57 @@
-// business logic
+// Business Logic
+
 function add(number1, number2) {
-    return number1 + number2;
-  }
-
-function mult(number1, number2) {
-    return number1*number2;
+  return number1 + number2;
 }
 
-function div(number1, number2) {
-    return number1/number2;
+function subtract(number1, number2) {
+  return number1 - number2;
 }
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function calculate(number1, number2, operator) {
+    if (operator === "add") {
+      return add(number1, number2);
+    }
+    
+    else if (operator === "subtract") {
+      return subtract(number1, number2);
+    }
+
+    else if (operator === "multiply") {
+      return multiply(number1, number2);
+    }
+    else if (operator === "divide") {
+      return divide(number1, number2);
+    }
+}
+
+// User Interface Logic
+function handleSubmission(event) {
+  event.preventDefault();
   
-  // user interface logic 
-  const number1 = parseInt(prompt("Enter a number:"));
-  const number2 = parseInt(prompt("Enter another number:"));
+  const number1 = parseInt(document.querySelector("input#number1").value);
+  const number2 = parseInt(document.querySelector("input#number2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
   
-  window.alert("Added" + add(number1, number2));
-  window.alert("Multiplied" + mult(number1, number2));
-  window.alert("Divided" + div(number1, number2));
+  let result = calculate(number1, number2, operator);
+
+  document.getElementById("output").innerText = result;
+}
+
+function submit() {
+  const form = document.getElementById("calcForm");
+  form.addEventListener("submit", handleSubmission);
+}
+
+
+// Add event listener
+
+window.addEventListener("load", submit);
